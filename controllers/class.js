9b910592843877bf778d99f0d4a9aa3d328df116
Ima7moodz <imahmoodi@outlook.com>
2025-01-20 16:14:59 +0300

    Update logic

--- controllers/class.js
+++ controllers/class.js
@@ -33,7 +33,6 @@ router.get("/", async (req, res) => {
 })
 
 //get class By ID
-
 router.get("/:classId", async (req, res) => {
   try {
     const showClass = await Class.findById(req.params.classId)
@@ -41,6 +40,51 @@ router.get("/:classId", async (req, res) => {
       return res.status(404).json({ error: "Class not found." })
     }
     res.status(200).json(showClass)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+//update class
+router.put("/:classId", verifyToken, async (req, res) => {
+  try {
+    const showClass = await Class.findById(req.params.classId)
+    if (!showClass) {
+      return res.status(404).json({ error: "Class not found." })
+    }
+
+    if (
+      req.user.role === "trainer" &&
+      showClass.userId.toString() === req.user._id
+    ) {
+      const updateClass = await Class.findByIdAndUpdate(
+        req.params.classId,
+        req.body,
+        { new: true }
+      )
+      return res.status(200).json(updateClass)
+    }
+
+    return res.status(403).json({ error: "Access denied!!!" })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+//delete class
+router.delete("/:classId", verifyToken, async (req, res) => {
+  try {
+    if (req.user.role !== "trainer") {
+      return res.status(403).json({ error: "Access denied." })
+    }
+
+    const showClass = await Class.findById(req.params.classId)
+    if (!showClass) {
+      return res.status(404).json({ error: "Class not found." })
+    }
+
+    await Class.findByIdAndDelete(req.params.classId)
+    res.status(200).json({ message: "Class deleted successfully." })
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
